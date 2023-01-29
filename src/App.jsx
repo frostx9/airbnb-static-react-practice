4dc644c440e93eb8626014f4d7ed01d48645b865
Saumya Som <saumyasom92@gmail.com>
2023-01-29 11:57:58 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../component/Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>App Component</h1>
+    </div>
+  )
+}
